f456a4511f377daa355497c6e99ecdff5f9b0ceb
Joseph Joyce <joej326@gmail.com>
2017-10-24 16:29:55 -0700

    added routes

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
 
+import { Router, ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
@@ -9,10 +11,20 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   private servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload(){
+    // the navigate method can take an optional 2nd argument
+    // with a built in relativeTo that tells angular what to make the '/servers' path
+    //relative to. This enables the navigate method to behave like  the template routerLink
+    // where we get /servers/servers instead of just /servers
+    this.router.navigate(['/servers'], {relativeTo: this.route});
   }
 
 }
